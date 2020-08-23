0319a4e8fdd7acc294d208b788d3229507c9b78d
Bastien <bchevriaut@gmail.com>
2020-08-23 16:37:09 +0200

    local classes

--- learningJavaLanguage/advancedClasses/nested/Nested.java
+++ learningJavaLanguage/advancedClasses/nested/Nested.java
@@ -6,24 +6,23 @@ public class Nested {
 	// Nested classes that are declared static are called static nested classes.
 	// Non-static nested classes are called inner classes.
 
+	public int x = 0;
 
-    public int x = 0;
+	class FirstLevel {
 
-    class FirstLevel {
+		public int x = 1;
 
-        public int x = 1;
+		void methodInFirstLevel(int x) {
+			System.out.println("x = " + x);
+			System.out.println("this.x = " + this.x);
+			System.out.println("Nested.this.x = " + Nested.this.x);
+		}
+	}
 
-        void methodInFirstLevel(int x) {
-            System.out.println("x = " + x);
-            System.out.println("this.x = " + this.x);
-            System.out.println("Nested.this.x = " + Nested.this.x);
-        }
-    }
+	public static void main(String... args) {
+		Nested ns = new Nested();
+		Nested.FirstLevel fl = ns.new FirstLevel();
+		fl.methodInFirstLevel(23);
+	}
 
-    public static void main(String... args) {
-    	Nested ns = new Nested();
-    	Nested.FirstLevel fl = ns.new FirstLevel();
-        fl.methodInFirstLevel(23);
-    }
-    
 }
